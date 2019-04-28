c554605fb49d798c91ffd06286ddfac11b33c7a2
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-28 18:36:06 +0100

    similar based on passed product

--- app/algorithms/__init__.py
+++ app/algorithms/__init__.py
@@ -4,13 +4,15 @@ import pandas
 import math
 
 
-def knn(products):
+def knn(products, product):
     dataset = numpy.array(products)[:, -3:]
     model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=5, n_jobs=-1)
 
     model.fit(dataset)
 
-    predicted = model.kneighbors(numpy.array([[16.0, 10.0, 14.0]]), n_neighbors = 5 + 1)
+    target = numpy.array(product)[-3:]
+
+    predicted = model.kneighbors(numpy.array([target]), n_neighbors=5 + 1)
 
     recommended = []
     for id in list(predicted[1][0]):
--- app/views.py
+++ app/views.py
@@ -16,15 +16,20 @@ def similar(request, product_id):
     print("PRODUCT ID %s" % product_id)
 
     with connection.cursor() as cursor:
-        sql = '''
+        sql_all = '''
             SELECT product_id, product_width_cm, product_length_cm, product_height_cm  
             FROM olist.products 
             WHERE   product_width_cm is not null AND 
                     product_length_cm is not null AND 
                     product_height_cm is not null
         '''
-        cursor.execute(sql)
+        cursor.execute(sql_all)
         products = cursor.fetchall()
-    recommended = knn(products)
+
+        sql_product = '''select product_id, product_width_cm, product_length_cm, product_height_cm 
+                        from olist.products where product_id = %s'''
+        cursor.execute(sql_product, [product_id])
+        product = cursor.fetchone()
+    recommended = knn(products, product)
     return HttpResponse(recommended)
 
